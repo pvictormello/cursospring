8d4eb8b625034f99759b76086cad428867a875ec
Paulo Victor <paulovictor.joao@gmail.com>
2020-04-06 14:37:53 -0300

    Pedido, pagamento e estado pagamento

---
+++ src/main/java/com/paulovictor/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.paulovictor.cursomc.repositories;
+
+import com.paulovictor.cursomc.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
+
