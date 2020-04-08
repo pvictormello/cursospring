e70e45a9448d5992bfbda95178c9ac4c781da30d
Paulo Victor <paulovictor.joao@gmail.com>
2020-04-08 16:45:26 -0300

    Correcao script geracao de dados

--- src/main/java/com/paulovictor/cursomc/services/DBService.java
+++ src/main/java/com/paulovictor/cursomc/services/DBService.java
@@ -15,25 +15,26 @@ import java.util.Arrays;
 public class DBService {
 
     @Autowired
-    CategoriaRepository categoriaRepository;
+    private CategoriaRepository categoriaRepository;
     @Autowired
-    ProdutoRepository produtoRepository;
+    private ProdutoRepository produtoRepository;
     @Autowired
-    CidadeRepository cidadeRepository;
+    private EstadoRepository estadoRepository;
     @Autowired
-    EstadoRepository estadoRepository;
+    private CidadeRepository cidadeRepository;
     @Autowired
-    ClienteRepository clienteRepository;
+    private ClienteRepository clienteRepository;
     @Autowired
-    EnderecoRepository enderecoRepository;
+    private EnderecoRepository enderecoRepository;
     @Autowired
-    PedidoRepository pedidoRepository;
+    private PedidoRepository pedidoRepository;
     @Autowired
-    PagamentoRepository pagamentoRepository;
+    private PagamentoRepository pagamentoRepository;
     @Autowired
-    ItemPedidoRepository itemPedidoRepository;
+    private ItemPedidoRepository itemPedidoRepository;
 
     public void instantiateTestDatabase() throws ParseException {
+
         Categoria cat1 = new Categoria(null, "Informática");
         Categoria cat2 = new Categoria(null, "Escritório");
         Categoria cat3 = new Categoria(null, "Cama mesa e banho");
@@ -55,16 +56,16 @@ public class DBService {
         Produto p11 = new Produto(null, "Shampoo", 90.00);
 
         cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-        cat2.getProdutos().addAll(Arrays.asList(p2,p4));
-        cat3.getProdutos().addAll(Arrays.asList(p5,p6));
-        cat4.getProdutos().addAll(Arrays.asList(p1,p2,p3,p7));
+        cat2.getProdutos().addAll(Arrays.asList(p2, p4));
+        cat3.getProdutos().addAll(Arrays.asList(p5, p6));
+        cat4.getProdutos().addAll(Arrays.asList(p1, p2, p3, p7));
         cat5.getProdutos().addAll(Arrays.asList(p8));
-        cat6.getProdutos().addAll(Arrays.asList(p9,p10));
+        cat6.getProdutos().addAll(Arrays.asList(p9, p10));
         cat7.getProdutos().addAll(Arrays.asList(p11));
 
-        p1.getCategorias().addAll(Arrays.asList(cat1));
-        p2.getCategorias().addAll(Arrays.asList(cat1,cat2,cat4));
-        p3.getCategorias().addAll(Arrays.asList(cat1,cat4));
+        p1.getCategorias().addAll(Arrays.asList(cat1, cat4));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2, cat4));
+        p3.getCategorias().addAll(Arrays.asList(cat1, cat4));
         p4.getCategorias().addAll(Arrays.asList(cat2));
         p5.getCategorias().addAll(Arrays.asList(cat3));
         p6.getCategorias().addAll(Arrays.asList(cat3));
@@ -75,32 +76,32 @@ public class DBService {
         p11.getCategorias().addAll(Arrays.asList(cat7));
 
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
-        produtoRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
 
         Estado est1 = new Estado(null, "Minas Gerais");
         Estado est2 = new Estado(null, "São Paulo");
 
-        Cidade c1 = new Cidade(null, "Uberlândia",est1);
+        Cidade c1 = new Cidade(null, "Uberlândia", est1);
         Cidade c2 = new Cidade(null, "São Paulo", est2);
         Cidade c3 = new Cidade(null, "Campinas", est2);
 
         est1.getCidades().addAll(Arrays.asList(c1));
-        est1.getCidades().addAll(Arrays.asList(c2,c3));
+        est2.getCidades().addAll(Arrays.asList(c2, c3));
 
-        estadoRepository.saveAll(Arrays.asList(est1,est2));
-        cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+        estadoRepository.saveAll(Arrays.asList(est1, est2));
+        cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
         Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 
         cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
-        Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+        Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
         Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
 
         cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 
         clienteRepository.saveAll(Arrays.asList(cli1));
-        enderecoRepository.saveAll(Arrays.asList(e1,e2));
+        enderecoRepository.saveAll(Arrays.asList(e1, e2));
 
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
@@ -120,15 +121,15 @@ public class DBService {
 
         ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
         ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
-        ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 2, 800.00);
+        ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
 
-        ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+        ped1.getItens().addAll(Arrays.asList(ip1, ip2));
         ped2.getItens().addAll(Arrays.asList(ip3));
 
         p1.getItens().addAll(Arrays.asList(ip1));
-        p2.getItens().addAll(Arrays.asList(ip2));
-        p3.getItens().addAll(Arrays.asList(ip3));
+        p2.getItens().addAll(Arrays.asList(ip3));
+        p3.getItens().addAll(Arrays.asList(ip2));
 
-        itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
+        itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
     }
 }
