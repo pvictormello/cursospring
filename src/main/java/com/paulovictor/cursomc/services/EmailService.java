1c4ea4343d37c0bd593a888498d934e80463e517
Paulo Victor <paulovictor.joao@gmail.com>
2021-04-28 14:55:43 -0300

    Implementando email html

--- src/main/java/com/paulovictor/cursomc/services/EmailService.java
+++ src/main/java/com/paulovictor/cursomc/services/EmailService.java
@@ -3,10 +3,16 @@ package com.paulovictor.cursomc.services;
 import com.paulovictor.cursomc.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 }
