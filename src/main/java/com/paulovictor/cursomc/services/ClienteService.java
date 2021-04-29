1e0e6fbf0340cddf2f94f83c49b638fc562d9fcc
Paulo Victor <paulovictor.joao@gmail.com>
2021-04-29 15:03:03 -0300

    ajustando tamanho da imagem

--- src/main/java/com/paulovictor/cursomc/services/ClienteService.java
+++ src/main/java/com/paulovictor/cursomc/services/ClienteService.java
@@ -43,6 +43,8 @@ public class ClienteService {
     private ImageService imageService;
     @Value("${img.prefix.client.profile}")
     private String prefix;
+    @Value("${img.profile.size}")
+    private Integer size;
 
 
     public Cliente find(Integer id) {
@@ -121,6 +123,8 @@ public class ClienteService {
         }
 
         BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+        jpgImage = imageService.cropSquare(jpgImage);
+        jpgImage = imageService.resize(jpgImage, size);
         String fileName = prefix + user.getId() + ".jpg";
 
         return s3Service.uplodadFile(imageService.getInputSteam(jpgImage, "jpg"), fileName, "image");
