630e0f8fef0c5ed0a98d9e73ae87bef5e0e17cf0
Paulo Victor <paulovictor.joao@gmail.com>
2021-04-29 13:38:56 -0300

    salvando primeiro arquivo no s3

--- src/main/java/com/paulovictor/cursomc/CursomcApplication.java
+++ src/main/java/com/paulovictor/cursomc/CursomcApplication.java
@@ -4,6 +4,7 @@ import com.paulovictor.cursomc.domain.*;
 import com.paulovictor.cursomc.domain.enums.EstadoPagamento;
 import com.paulovictor.cursomc.domain.enums.TipoCliente;
 import com.paulovictor.cursomc.repositories.*;
+import com.paulovictor.cursomc.services.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,12 +16,15 @@ import java.util.Arrays;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+    @Autowired
+    private S3Service s3Service;
+
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        s3Service.uplodadFile("C:\\Users\\paulo\\Pictures\\Camera Roll\\WIN_20210413_13_27_13_Pro.jpg");
     }
 }
